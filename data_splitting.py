25a741db173e7e7c9ac440d0e8bea4435c359818
IHiroaki2 <hiro0108991@gmail.com>
2024-07-19 12:14:07 +0900

    first commit

--- data_splitting.py
+++ data_splitting.py
@@ -12,16 +12,16 @@ import numpy as np
 
 from config import CFG
 
-columns = pl.read_csv(f"{CFG.MAIN_FOLDER}leap-atmospheric-physics-ai-climsim/train.csv", n_rows = 1).to_pandas().columns.to_list()
+columns = pl.read_csv(f"leap-atmospheric-physics-ai-climsim/train.csv", n_rows = 1).to_pandas().columns.to_list()
 
 norm_data = pd.DataFrame(np.zeros((556+368, 2)), columns=["mean", "std"])
 
 for i in range(0, len(CFG.start_num)):
     print(i)
     cols = columns[CFG.start_num[i] : CFG.end_num[i]]
-    df = pl.read_csv(f"{CFG.MAIN_FOLDER}leap-atmospheric-physics-ai-climsim/train.csv", columns = cols).to_pandas()
+    df = pl.read_csv(f"leap-atmospheric-physics-ai-climsim/train.csv", columns = cols).to_pandas()
     if i >= 10:
-        sub = pl.read_csv(f"{CFG.MAIN_FOLDER}leap-atmospheric-physics-ai-climsim/sample_submission.csv", columns=cols, n_rows=1).to_numpy()
+        sub = pl.read_csv(f"leap-atmospheric-physics-ai-climsim/sample_submission.csv", columns=cols, n_rows=1).to_numpy()
         df = df*sub.reshape((1, -1))
     
     m = df.iloc[CFG.N:, :].mean(axis=0).values
@@ -44,6 +44,6 @@ for i in range(0, len(CFG.start_num)):
     norm_data.iloc[CFG.start_num[i]-1:CFG.end_num[i]-1, 1] = s
     
     for n in range(10):
-        df.iloc[CFG.N*n:CFG.N*(n+1), :].to_parquet(f"{CFG.MAIN_FOLDER}data/data_{i}_{n}.parquet")
+        df.iloc[CFG.N*n:CFG.N*(n+1), :].to_parquet(f"data/data_{i}_{n}.parquet")
     
-norm_data.to_parquet(f"{CFG.MAIN_FOLDER}data/norm_data.parquet")
+norm_data.to_parquet(f"data/norm_data.parquet")
